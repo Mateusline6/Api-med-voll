5abbab471613a3a0b04d318e14e5b1e93034b99e
MateusPadial <ryuksilva@gmail.com>
2024-07-16 16:24:16 -0300

    feat: Tratativa de erros

--- api--med-voll/src/main/java/med/voll/apimedvoll/infra/security/SecurityFilter.java
+++ api--med-voll/src/main/java/med/voll/apimedvoll/infra/security/SecurityFilter.java
@@ -23,15 +23,13 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("Chamando filter");
+
         var tokenJWT = recuperarToken(request);
         if (tokenJWT != null){
             var token = tokenService.getSubject(tokenJWT);
             var usuario = usuarioRepository.findByLogin(token);
             var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            System.out.println("logado");
 
         }
         filterChain.doFilter(request, response);
